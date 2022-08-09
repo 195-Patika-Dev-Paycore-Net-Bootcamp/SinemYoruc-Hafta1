e1cca4bb4f6574c78d10e54fba4bdb9712b0f778
sinemyoruc <sinemyoruc@gmail.com>
2022-08-09 15:26:04 +0300

    Interest added

---
+++ Odev1/Controllers/InterestController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Odev1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InterestController : ControllerBase
+    {
+        [HttpGet("GetTotalBalance")]
+        public ActionResult<Interest> Get([FromQuery] double principal, double interestAmount, double expiry) //Request body (ana para, faiz orani ve vade alindi)
+        {
+            Interest interest = new(); //yeni liste
+            interest.InterestAmount = interestAmount; //listenin elemani InterestAmount ile kullanicidan alinan interestAmount eslestirildi
+            interest.TotalBalance = principal * Math.Pow((1 + interestAmount), expiry); //bilesik faiz hesabi
+            return interest; //response body (toplam bakiye ve faiz orani donduruldu)
+        }
+    }
+}
---
+++ Odev1/Interest.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Odev1
+{
+    public class Interest
+    {
+        public double InterestAmount { get; set; } //Faiz Oraný
+
+        public double TotalBalance { get; set; } //Anapara
+    }
+}
