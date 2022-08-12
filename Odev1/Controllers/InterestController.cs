7d4ee1e699bdb1b2d6863334aeec5e02bf8b782c
sinemyoruc <sinemyoruc@gmail.com>
2022-08-12 15:14:34 +0300

    interestRate and interestAmount updated

--- Odev1/Controllers/InterestController.cs
+++ Odev1/Controllers/InterestController.cs
@@ -8,11 +8,14 @@ namespace Odev1.Controllers
     public class InterestController : ControllerBase
     {
         [HttpGet("GetInterest")]
-        public ActionResult<Interest> Get([FromQuery] double principal, double interestAmount, double expiry) //Request body (ana para, faiz orani ve vade alindi)
+        public ActionResult<Interest> Get([FromQuery] double principal, double interestRate, double expiry) //Request body (ana para, faiz orani ve vade alindi)
         {
             Interest interest = new(); //yeni liste
-            interest.InterestAmount = interestAmount; //listenin elemani InterestAmount ile kullanicidan alinan interestAmount eslestirildi
-            interest.TotalBalance = principal * Math.Pow((1 + interestAmount), expiry); //bilesik faiz hesabi
+            double totalBalance = principal * Math.Pow((1 + interestRate), expiry); //bilesik faiz hesabi
+            double interestAmount = totalBalance - principal;  //faiz getirisi hesabi
+            interest.TotalBalance = totalBalance;  //Interest sinifindaki TotalBalance ile InterestController sinifindaki totalBalance parametreleri esitlendi
+            interest.InterestAmount = interestAmount; //Interest sinifindaki InterestAmount ile InterestController sinifindaki interestAmount parametreleri esitlendi
+            interest.InterestRate = interestRate; //Interest sinifindaki InterestRate ile InterestController sinifindaki interestRate parametreleri esitlendi
             return interest; //response body (toplam bakiye ve faiz orani donduruldu)
         }
     }
--- Odev1/Interest.cs
+++ Odev1/Interest.cs
@@ -4,8 +4,10 @@ namespace Odev1
 {
     public class Interest
     {
-        public double InterestAmount { get; set; } //Faiz Oraný
+        public double InterestAmount { get; set; } //Faiz Getirisi
 
         public double TotalBalance { get; set; } //Anapara
+
+        public double InterestRate { get; set; } //Faiz Orani
     }
 }
