7425b25f6b998ec66b7ca56afa106d6eadb7e8d4
sinemyoruc <sinemyoruc@gmail.com>
2022-08-13 14:52:44 +0300

    Validation added

--- Odev1/Controllers/InterestController.cs
+++ Odev1/Controllers/InterestController.cs
@@ -11,12 +11,19 @@ namespace Odev1.Controllers
         public ActionResult<Interest> Get([FromQuery] double principal, double interestRate, double expiry) //Request body (ana para, faiz orani ve vade alindi)
         {
             Interest interest = new(); //yeni liste
-            double totalBalance = principal * Math.Pow((1 + interestRate), expiry); //bilesik faiz hesabi
-            double interestAmount = totalBalance - principal;  //faiz getirisi hesabi
-            interest.TotalBalance = totalBalance;  //Interest sinifindaki TotalBalance ile InterestController sinifindaki totalBalance parametreleri esitlendi
-            interest.InterestAmount = interestAmount; //Interest sinifindaki InterestAmount ile InterestController sinifindaki interestAmount parametreleri esitlendi
-            interest.InterestRate = interestRate; //Interest sinifindaki InterestRate ile InterestController sinifindaki interestRate parametreleri esitlendi
-            return interest; //response body (toplam bakiye ve faiz orani donduruldu)
+            if (principal > 0 && interestRate > 0 && expiry > 0) //girilen degerlerin eksi veya 0 olup olmadigi kontrol edildi
+            {
+                double totalBalance = principal * Math.Pow((1 + interestRate), expiry); //bilesik faiz hesabi
+                double interestAmount = totalBalance - principal;  //faiz getirisi hesabi
+                interest.TotalBalance = totalBalance;  //Interest sinifindaki TotalBalance ile InterestController sinifindaki totalBalance parametreleri esitlendi
+                interest.InterestAmount = interestAmount; //Interest sinifindaki InterestAmount ile InterestController sinifindaki interestAmount parametreleri esitlendi
+                interest.InterestRate = interestRate; //Interest sinifindaki InterestRate ile InterestController sinifindaki interestRate parametreleri esitlendi
+                return Ok(interest); //response body (toplam bakiye, faiz getirisi ve faiz orani donduruldu)
+            }
+            else
+            {
+                return BadRequest("Lütfen 0'dan büyük değerler giriniz!!"); //400 kodu ve hata mesaji donduruldu
+            }
         }
     }
 }
